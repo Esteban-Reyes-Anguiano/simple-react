d4f86f0344fc5d2dafc4516148405a2fb2796f65
Esteban-Reyes-Anguiano <139920025+Esteban-Reyes-Anguiano@users.noreply.github.com>
2023-07-19 15:44:16 -0600

    cambios en proyecto

---
+++ simple-app/src/App.js
@@ -0,0 +1,19 @@
+import React, { useState } from "react";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  const increment = () => {
+    setCount(count + 1);
+  };
+
+  return (
+    <div>
+      <h1>Simple React App</h1>
+      <p>Count: {count}</p>
+      <button onClick={increment}>Increment</button>
+    </div>
+  );
+}
+
+export default App;
